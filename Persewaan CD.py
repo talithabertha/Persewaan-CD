1074ffd418b56a56536b018b97745b1d0e00d3d6
unknown <revyandana@gmail.com>
2022-05-29 23:09:05 +0700

    Mengerjakkan Proyek Prokom

--- Persewaan CD.py
+++ Persewaan CD.py
@@ -31,3 +31,43 @@ def cek_login_member():
         login_member()
     else:
         register()
+
+def login_member():
+    file = "database.xlsx"
+    database= pd.read_excel(file,sheet_name="Data Member")
+    df=pd.DataFrame(database)
+    
+    print("============================================")
+    user = str(input("Username: "))
+    password = str(input("Password: "))
+    if  user in list(df['Username'].values):
+        if password in list(df['Password'].values):
+            print("============================================")
+            print("Login sukses")
+        else:
+            ("============================================")
+            print("Password salah")
+            return login_member()
+    else:
+        ("============================================")
+        print("Username tidak terdaftar")
+        return login_member()
+
+def login_admin():
+    print("============================================")
+    while True:
+        admin = str(input("Username : "))
+        passadmin = str(input("Password : "))
+        if admin == "admin":
+            if passadmin == "admin":
+                print("============================================")
+                print("Login sukses!")
+                menu_admin()
+                break
+            else:
+                print("============================================")
+                print("Password salah!")
+        else:
+            print("============================================")
+            print("Username salah!")
+            print("============================================")
